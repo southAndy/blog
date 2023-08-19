664e66d7de6d361712c4b94d46a9b734992531e6
southAndy <zatchbell.1206@gmail.com>
2023-08-19 13:48:50 +0800

    feat(project): add navbar setting

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: "home", link: "/" },
+      { text: "about", link: "/about/" },
+
+      {
+        text: "guide",
+        link: "/guide/",
+      },
+    ],
+  },
+};
