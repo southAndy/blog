849ae2b071a8923e5aee84b2c627ca523088310b
southAndy <zatchbell.1206@gmail.com>
2024-01-08 16:59:56 +0800

    refactor(article) modify the structure of articles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,16 +11,22 @@ module.exports = {
         link: "/archives/fe/",
         items: [
           { text: "打包", link: "/archives/fe/bundler/" },
-          { text: "Javascript", link: "/archives/fe/javascript" },
+          { text: "Javascript", link: "/archives/fe/js/" },
+          { text: "vuejs", link: "/archives/fe/vuejs/" },
         ],
       },
       {
         text: "後端",
         link: "/archives/be/",
+        items: [
+          { text: "Nodejs", link: "/archives/be/nodejs/" },
+          { text: "資料庫", link: "/archives/be/database/" },
+          { text: "爬蟲", link: "/archives/be/crawler/" },
+        ],
       },
       {
         text: "其他文章",
-        link: "/archives/other/",
+        link: "/archives/others/",
       },
       {
         text: "關於我",
